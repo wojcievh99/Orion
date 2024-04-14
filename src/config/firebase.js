f9258f88ddd6863192afcd75ddb1c9e48bfcfb0d
wojcievh99 <155263497+wojcievh99@users.noreply.github.com>
2024-04-14 20:52:02 +0200

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,14 +4,9 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
+//
 const firebaseConfig = {
-  apiKey: "AIzaSyAT0ln4PsdqQQ_kdZZWiTRuF7VsVaeNfLA",
-  authDomain: "orion-6352e.firebaseapp.com",
-  projectId: "orion-6352e",
-  storageBucket: "orion-6352e.appspot.com",
-  messagingSenderId: "739582674828",
-  appId: "1:739582674828:web:322dbc63c00af37c9078a9",
-  measurementId: "G-4GFVKLCM62"
+  ///***
 };
 
 // Initialize Firebase
